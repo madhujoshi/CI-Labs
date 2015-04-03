f856b754caa551f70d77ee28421485f0822345ff
Ubuntu <ubuntu@ip-172-30-1-207.us-west-1.compute.internal>
2015-04-03 00:42:56 +0000

    initial checkin

---
+++ src/test/java/TempConv/AppTest.java
@@ -0,0 +1,26 @@
+package TempConv;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite; 
+import org.junit.Before;
+import junit.framework.Assert;
+import junit.framework.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+
+    public void test1App()
+    {
+        System.out.println("Test1 passed!" );
+    }
+
+    public void test2App()
+    {
+        System.out.println("Test2 passed!" );
+    }
+
+
+}
